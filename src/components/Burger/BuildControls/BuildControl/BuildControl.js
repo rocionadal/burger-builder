710f9bc2246f961c56ecfeabe48991000cd67472
rocionadal <rocioonadal@gmail.com>
2021-03-01 19:36:31 +0100

    apply classes to js

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import classes from './buildControl.css';
+
 const buildControl = (props) => {
-    <div>
-        <div>{props.label}</div>
-        <button>Less</button>
-        <button>More</button>
+    <div className={classes.BuildControl}>
+        <div className={classes.Label}>{props.label}</div>
+        <button className={classes.Less}>Less</button>
+        <button className={classes.More}>More</button>
     </div>
 }
 
