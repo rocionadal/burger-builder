affbb246fbbc4236e69e75948d1f9bea6f58986e
rocionadal <rocioonadal@gmail.com>
2021-03-01 21:41:25 +0100

    function and css style for backdrop

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import classes from './Modal.css'
+import Aux from '../../../hoc/Aux';
+import Backdrop from '../Backdrop/Backdrop';
 
 const modal = (props) => (
     <div 
