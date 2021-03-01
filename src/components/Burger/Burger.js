e4895db779e699087b2d66d1c231f2789cf134dd
rocionadal <rocioonadal@gmail.com>
2021-03-01 19:13:15 +0100

    transform object of ingredients into array to see how many do i need

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,6 +4,12 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients) // keys will return an array with the keys of the obj as strings
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, 1) => { // array with 2 elements
+                return <BurgerIngredient key={igKey + i} type={igKey} />;
+            }); 
+        });
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
