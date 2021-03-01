d19fea79b85ad2767d545e8637c000372f8d3138
rocionadal <rocioonadal@gmail.com>
2021-03-01 19:31:15 +0100

    folder & file for build control

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const buildControl = () => {
+
+}
+
+export default buildControl;
--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const buildControls = (props) => {
-
+    <div>
+        
+    </div>
 };
 
 export default buildControls;
