0a381c4dc1fabd8b96f8f3bcd2c91d3cf6788c17
rocionadal <rocioonadal@gmail.com>
2021-03-01 22:27:28 +0100

    implementing button type component

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -5,7 +5,7 @@ import classes from './Button.css';
 const button = (props) => (
     <button
     className={[classes.Button, classes[props.btnType]].join(' ')}
-    onClick={props.children}>{props.children}</button>
+    onClick={props.clicked}>{props.children}</button>
 );
 
 export default button;
