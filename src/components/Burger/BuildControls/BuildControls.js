2b47b6117c7b2bf212308aa0cf9100e15255c6bc
rocionadal <rocioonadal@gmail.com>
2021-03-01 20:22:58 +0100

    disable less button if we dont have ingredients

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -17,7 +17,8 @@ const buildControls = (props) => (
                 key={ctrl.label} 
                 label={ctrl.label}
                 added={() => props.ingredientAdded(ctrl.type)}
-                removed={() => props.ingredientRemove(ctrl.type)} />
+                removed={() => props.ingredientRemove(ctrl.type)}
+                disabled={props.disabled[ctrl.type]} />
         ))}
     </div>
 );
